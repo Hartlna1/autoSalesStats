aba4726764d0310fd39021d9342548682bb91ffa
Hartlna1 <nan.hartley@gmail.com>
2013-10-08 17:30:05 -0400

    wrote additional code to finish program
    UNTESTED; user inputs, processing, output and finish of program
    including fixing to two decimal places.

--- autoSalesStats.js
+++ autoSalesStats.js
@@ -1,4 +1,4 @@
-/* BE SURE TO DO A SAVE AS WHEN THIS IS OPENED!
+/* 
 Programmer: 	Nanette K. Hartley
 Language: 		JavaScript
 Filename:  		autoSalesStats.js
@@ -22,9 +22,22 @@ NAME = "Bennet's Used Auto Sales";		//name of the dealership
 document.write("Welcome to" + ES + NAME + "!" + ES + ES + "This program will calculate the yearly total sales and the average quarterly sales from the sales per quarter." + BR + BR);
 
 //Getting user input
+quarterOne = prompt("In order to calculate the average quarterly sales and the total sales for the year, I need the quarter one sales:");
+quarterOne = parseFloat(quarterOne);
+quarterTwo = prompt("Quarter two sales:");
+quarterTwo = parseFloat(quarterTwo);
+//quarterThree = prompt("Quarter three sales:");
+//quarterThree = parseFloat(quarterThree);
+//quarterFour = prompt("Quarter four sales:");
+//quarterFour = parseFloat(quarterFour);
 
 //Processing input to provide output 
+//total = quarterOne + quarterTwo + quarterThree+ quarterFour;
+//average = total/4;
 
 //Output of data
+//document.write("Given quarterly sales of" + ES + quarterOne.toFix(2) + "," + ES + quarterTwo.toFix(2) + "," + ES + quarterThree.toFix(2) + "," + ES + quarterFour.toFix(2) + "," + ES "the yearly total is:" + ES + total.toFix(2) + LE + BR);
+//document.write("The quarterly average is:" + ES + average.toFix(2) + LE + BR + BR);
 
 //Thank the user and end the program
+//document.write("Thank you for using our program!" + BR);
