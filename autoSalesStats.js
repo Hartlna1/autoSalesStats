a8c4a0811dc574725d6177f71136759d309a475a
Hartlna1 <nan.hartley@gmail.com>
2013-10-08 16:25:50 -0400

    Initial commit of program.
    variables are defined and program information has been added.  No
    processing or output is yet defined in the word document, the .js file,
    or the .html file.

---
+++ autoSalesStats.js
@@ -0,0 +1,30 @@
+/* BE SURE TO DO A SAVE AS WHEN THIS IS OPENED!
+Programmer: 	Nanette K. Hartley
+Language: 		JavaScript
+Filename:  		autoSalesStats.js
+Date: 			10/8/2013
+Description: 	Calculates the total of the quarterly sales for the year, and then outputs the average of the sales.  
+*/
+
+//declaration of variables and constants
+var BR = "<br />";						//HTML line break
+var ES = " ";							//HTML extra space
+var LE = ".";							//HTML line end
+var quarterOne = 0.0;					//Quarter one sales
+var quaterTwo = 0.0; 					//Quarter two sales
+var quarterThree = 0.0;					//Quarter three sales
+var quarterFour = 0.0;					//Quarter four sales
+total = 0.0;							//total of sales
+average = 0.0;							//average of quarterly sales
+NAME = "Bennet's Used Auto Sales";		//name of the dealership
+
+//Welcome the user to the program
+document.write("Welcome to" + ES + NAME + "!" + ES + ES + "This program will calculate the yearly total sales and the average quarterly sales from the sales per quarter." + BR + BR);
+
+//Getting user input
+
+//Processing input to provide output 
+
+//Output of data
+
+//Thank the user and end the program
