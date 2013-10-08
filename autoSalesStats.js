218a618f117e6cc096db9aa30458257a55dc8818
Hartlna1 <nan.hartley@gmail.com>
2013-10-08 17:38:19 -0400

    tested output line 2

--- autoSalesStats.js
+++ autoSalesStats.js
@@ -37,7 +37,7 @@ average = total/4;
 
 //Output of data
 document.write("Given quarterly sales of $" + quarterOne.toFixed(2) + ", $" + quarterTwo.toFixed(2) + ", $" + quarterThree.toFixed(2) + ", $" + quarterFour.toFixed(2) + "," + ES + "the yearly total is: $" + total.toFixed(2) + LE + BR);
-//document.write("The quarterly average is:" + ES + average.toFixed(2) + LE + BR + BR);
+document.write("The quarterly average is: $" + average.toFixed(2) + LE + BR + BR);
 
 //Thank the user and end the program
 //document.write("Thank you for using our program!" + BR);
